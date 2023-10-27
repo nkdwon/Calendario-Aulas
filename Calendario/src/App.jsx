cf5d464b5fa6251fb916018cb78b2900f9700a78
Peraltha <99258500+Peraltha@users.noreply.github.com>
2023-10-27 08:06:41 -0300

    Css inicial

--- Calendario/src/App.jsx
+++ Calendario/src/App.jsx
@@ -1,17 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Calendário</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
     </>
   )
 }
