923da308b42d6045913d8fd38900d02283d63a38
Matheus Amaral Lara <matheuslara1195@gmail.com>
2023-10-31 09:39:38 -0300

    Dropdown

--- Calendario/components/TopBar/index.jsx
+++ Calendario/components/TopBar/index.jsx
@@ -6,14 +6,16 @@ export default function Topbar() {
             <div className={styles.navbar}>
                 <div className={styles.titulo}>
                     <h1>Agenda Online</h1>
-                    <div class="dropdown">
-                        <button class="dropbtn">Dropdown</button>
-                        <div class="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
+
+                    <div className={styles.dropdown}>
+                        <button className={styles.dropbtn}>Menu</button>
+                        <div className={styles.dropdowncontent}>
+                            <a href="">Link 1</a>
+                            <a href="">Link 2</a>
+                            <a href="">Link 3</a>
                         </div>
                     </div>
+
                 </div>
             </div>
         </>
